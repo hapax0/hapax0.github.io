2b3efec4c33f5406280570f6733821ddaa9aa1f9
* <Paul Duffy>
2022-11-26 05:55:56 -0500

    moby

---
+++ emily.js
@@ -0,0 +1,29 @@
+
+let emily="If you were coming in the fall,\\n\
+I'd brush the summer by\\n\
+With half a smile and half a spurn,\\n\
+As housewives do a fly.\\n\
+\\n\
+\\n\
+If I could see you in a year,\\n\
+I'd wind the months in balls,\\n\
+And put them each in separate drawers,\\n\
+Until their time befalls.\\n\
+\\n\
+\\n\
+If only centuries delayed,\\n\
+I'd count them on my hand,\\n\
+Subtracting till my fingers dropped\\n\
+Into Van Diemen's land.\\n\
+\\n\
+\\n\
+If certain, when this life was out,\\n\
+That yours and mine should be,\\n\
+I'd toss it yonder like a rind,\\n\
+And taste eternity.\\n\
+\\n\
+\\n\
+But now, all ignorant of the length\\n\
+Of time's uncertain wing,\\n\
+It goads me, like the goblin bee,\\n\
+That will not state its sting.\\n"
