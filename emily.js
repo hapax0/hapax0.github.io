816fded35d64eed842fbe38b8cd7433eb370adf4
* <Paul Duffy>
2022-11-26 13:36:38 -0500

    moby

--- emily.js
+++ emily.js
@@ -23,3 +23,57 @@ But now, all ignorant of the length\\n\
 Of time's uncertain wing,\\n\
 It goads me, like the goblin bee,\\n\
 That will not state its sting.\\n"
+
+let crossword="HORIZONTAL\\n\
+\\n\
+ 1 Pronoun\\n\
+ 3 Albumin from castor-oil bean\\n\
+ 7 Exist\\n\
+ 9 Aged\\n\
+11 Negative\\n\
+12 Incite, hasten\\n\
+13 Remote\\n\
+15 Obstruction\\n\
+17 Bivalves\\n\
+21 Father\\n\
+23 Tree\\n\
+24 River in Italy\\n\
+25 Owners\\n\
+26 Printer’s measure\\n\
+27 Tree\\n\
+28 Personal pronoun\\n\
+29 Legislative bodies\\n\
+31 Compact mass\\n\
+32 Moved rapidly\\n\
+34 Walk about\\n\
+35 Toss\\n\
+37 Small child\\n\
+39 Upon\\n\
+40 Small openings\\n\
+VERTICAL\\n\
+\\n\
+ 1 Exclamation\\n\
+ 2 Fairy\\n\
+ 4 Preposition\\n\
+ 5 Plotter\\n\
+ 6 Pronoun\\n\
+ 7 Express generally\\n\
+ 8 Pronoun\\n\
+10 Obstruct\\n\
+12 Owns\\n\
+14 Disarranged\\n\
+15 Voluble talkativeness\\n\
+16 Above\\n\
+18 The bow of Vishnu\\n\
+19 Choose\\n\
+20 Assumed an attitude\\n\
+22 Limb\\n\
+24 Peer\\n\
+29 Sorrowful\\n\
+30 Rested\\n\
+31 Pale\\n\
+33 Incline the head\\n\
+34 Move\\n\
+35 Behold\\n\
+36 Exist\\n\
+38 Preposition"
